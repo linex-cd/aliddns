5f7a1b618a1782aa1357838092d597e460126ea3
Linex <26711215+linex-cd@users.noreply.github.com>
2022-12-27 18:14:37 +0800

    修复curl的命令问题

---
+++ aliddns.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
+	log "github.com/sirupsen/logrus"
+)
+
+type CONFIG struct {
+	AccessKeyId  string `json:"accessKeyId"`
+	AccessSecret string `json:"accessSecret"`
+	RecordID     string `json:"RecordID"`
+}
+
+var Config CONFIG
+
+func main() {
+	file, _ := os.Open("./config.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+
+	err := decoder.Decode(&Config)
+	if err != nil {
+		panic(err)
+	}
+
+	go SetDDNSService()
+	for {
+		time.Sleep(time.Duration(60) * time.Second)
+	}
+}
+
+func SetDDNSService() {
+	var WanIP string
+	var RecordIP string
+	var RecordRR string
+
+	RecordIP, RecordRR = GetAliRecordIP() // 服务器启动时，从阿里云获取一次
+
+	for {
+		WanIP = GetWanIPStr()
+		log.Info("Get WAN IP: ", WanIP)
+		if WanIP != "" && WanIP != RecordIP {
+			log.Info("Wan IP changed. Will change the record IP.")
+			err := SetDDNS(RecordRR, WanIP)
+			if err == nil {
+				RecordIP = WanIP
+			}
+
+		} else {
+			//log.Info("Wan IP hold.")
+		}
+		time.Sleep(time.Duration(60) * time.Second)
+	}
+}
+
+func SetDDNS(RecordRR string, wanIP string) (err error) {
+	client, err := alidns.NewClientWithAccessKey("cn-hangzhou", Config.AccessKeyId, Config.AccessSecret)
+
+	request := alidns.CreateUpdateDomainRecordRequest()
+	request.Scheme = "https"
+
+	request.RecordId = Config.RecordID
+	request.RR = RecordRR
+	request.Type = "A"
+	request.Value = wanIP //GetWanIPStr() //"118.123.37.212"
+	request.Lang = "en"
+	request.UserClientIp = wanIP // "118.123.37.211"
+	request.TTL = "600"
+	request.Priority = "1"
+	request.Line = "default"
+
+	response, err := client.UpdateDomainRecord(request)
+	if err != nil {
+		fmt.Print(err.Error(), response)
+		return err
+	}
+	fmt.Printf("response is %#v\n", response)
+	return nil
+}
+
+func GetAliRecordIP() (recordIP string, recordRR string) {
+	client, err := alidns.NewClientWithAccessKey("cn-hangzhou", Config.AccessKeyId, Config.AccessSecret)
+
+	request := alidns.CreateDescribeDomainRecordInfoRequest()
+	request.Scheme = "https"
+
+	request.RecordId = Config.RecordID
+	request.Lang = "en"
+	request.UserClientIp = ""
+
+	response, err := client.DescribeDomainRecordInfo(request)
+	if err != nil {
+		fmt.Print(err.Error())
+		return "", ""
+	}
+	log.Info("Record IP: ", response.Value)
+	return response.Value, response.RR
+}
+
+func GetWanIPStr() (wanip string) {
+	cmd := exec.Command("curl", "ident.me")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Error("error: ", err)
+		return ""
+	}
+	//fmt.Printf("in all caps: %q\n", out.String())
+
+	wanip = out.String()
+	if wanip != "" {
+		//log.Info("Get WAN IP ok: ", wanip)
+	} else {
+		log.Warn("Get WAN IP failed")
+	}
+	return wanip
+}
--- readme.md
+++ readme.md
@@ -1,9 +1,19 @@
-#使用说明
+# 使用说明
 
-1.把config.json-模板文件重命名为config.json
+1.把config.json-模板文件重命名为config.json。
 
-2.在阿里云控制台申请accesskey，并在config.json中填写对应的id和secret
+2.在阿里云控制台申请accesskey，并在config.json中填写对应的id和secret。
 
 3.在域名控制台中，在需要解析的域名的解析页，手动添加一个解析记录，IP随便写。然后打开Chrome的网络调试。刷新页面，在Network找dns/rr_api/list.json（版本更新可能会是其他的请求地址）。找到对应的解析记录的recordId，然后更新到config.json中。
 
-4.启动exe即可。
+4.如果是windows系统，请先安装curl。如果是mac/linux，可能已经有curl了。请手动确认是否安装。
+
+5.启动aliddns（.exe）即可。
+
+# 编译说明
+
+1. go get 安装依赖。如果连不上服务器，先go env -w GOPROXY=https://goproxy.cn使用代理。
+
+2. go run aliddns.go 测试程序。
+
+3. go build aliddns.go 编译得到二进制程序。
